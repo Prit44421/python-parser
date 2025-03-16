a6d7da763c718c9b8d02773fc032778afa030b03
Prit44421 <pritgambhwa@gmail.com>
2025-03-16 22:05:15 +0530

    added few more

--- parser.cpp
+++ parser.cpp
@@ -5,14 +5,19 @@ using namespace std;
 // Token types
 typedef enum {
     KEYWORD_TOKEN,              // keyword token
-    LBRACE_TOKEN,               // "(" left brace token 
-    RBRACE_TOKEN,               // ")" right brace token
+    CURVE_LBRACE_TOKEN,               // "(" left brace token 
+    CURVE_RBRACE_TOKEN,               // ")" right brace token
+    SQUARE_LBRACE_TOKEN,               // "[" left brace token 
+    SQUARE_RBRACE_TOKEN,               // "]" right brace token
+    CURLY_LBRACE_TOKEN,               // "{" left brace token 
+    CURLY_RBRACE_TOKEN,               // "}" right brace token
     ID_TOKEN,                   // id token
     NUM_TOKEN,                  // num token
     OPERATOR_TOKEN,             // +,-,/,*,%,** operator token
     COMPARE_OPERATOR_TOKEN,     // ==,>=,<=
-    COLON_TOKEN                 // : 
-
+    COLON_TOKEN  ,               // : 
+    INVALID_TOKEN,              // invalid token
+    COMPARE_OPERATOR_TOKEN      // ==,>=,<=,!=
 } TokenType;
 
 // Token structure
@@ -27,13 +32,13 @@ typedef struct Token{
 } Token;
 
 // check if the character is a start of an identifier
-bool is_id_start(char s){
-    if(s=='_'||isalpha(s)) return true;
-    return false;
-}
+// bool is_id_start(char s){
+//     if(s=='_'||isalpha(s)) return true;
+//     return false;
+// }
 // check if the character is an identifier
 bool is_id(char s){
-    if(s=='_',isalnum(s)) return true;
+    if(s=='_'||isalnum(s)) return true;
     return false;
 }
 // check if the character is a digit start
@@ -45,6 +50,38 @@ bool is_digit_start(char s){
 bool is_digit(char s){
     if(isdigit(s)) return true;
     return false;
+}
+// check if the character is an operator
+bool is_operator(char c) {
+    if ( (c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '%')){
+        return true;
+    }else{
+        return false;
+    }
+}
+// check if the character is a compare operator
+bool is_compare_operator(string c) {
+    if ( (c == "==") || (c == ">=") || (c == "<=") || (c == "!=") ){
+        return true;
+    }else{
+        return false;
+    }
+}
+// check if the character is a compare operator start
+bool is_compare_operator_start(char c) {
+    if ( (c == '=') || (c == '>') || (c == '<') || (c == '!') ){
+        return true;
+    }else{
+        return false;
+    }
+}
+// check if the character is a braces
+bool is_braces(char c) {
+    if ( (c == '{') || (c == '[') || (c == '(') || (c == ')') || (c == ']') || (c == '}') ){
+        return true;
+    }else{
+        return false;
+    }
 }
 // Keywords list
 vector<string> keywords = {
@@ -64,7 +101,7 @@ vector<Token> tokenizer(string s){
         string token="";
 
         // check if the next token is a keyword or an identifier
-        if(is_id_start(s[cursor])){
+        if(is_id(s[cursor])){
             token+=s[cursor];
             cursor++;
             bool keyword_flag=true;
@@ -95,6 +132,61 @@ vector<Token> tokenizer(string s){
             }
             tokens.push_back(Token(token,NUM_TOKEN));
         }
+        else if (is_operator(s[cursor])){
+            token+=s[cursor];
+            cursor++;
+            while(cursor<s.size()&&is_operator(s[cursor])){
+                token+=s[cursor];
+                cursor++;
+            }
+            if(token.size()==1 ||  token=="**"){
+                tokens.push_back(Token(token,OPERATOR_TOKEN));
+            }
+            else{
+                tokens.push_back(Token(token,INVALID_TOKEN));
+            }
+        }
+        else if(is_compare_operator_start(s[cursor])){
+            token+=s[cursor];
+            cursor++;
+            while(cursor<s.size()&&is_compare_operator_start(s[cursor])){
+                token+=s[cursor];
+                cursor++;
+            }
+            if(is_compare_operator(token)){
+                tokens.push_back(Token(token,COMPARE_OPERATOR_TOKEN));
+            }
+            else{
+                tokens.push_back(Token(token,INVALID_TOKEN));
+            }
+        }
+        else if(is_braces(s[cursor])){
+            if(s[cursor]=='{'){
+                tokens.push_back(Token("{",CURLY_LBRACE_TOKEN));
+            }
+            else if(s[cursor]=='}'){
+                tokens.push_back(Token("}",CURLY_RBRACE_TOKEN));
+            }
+            else if(s[cursor]=='['){
+                tokens.push_back(Token("[",SQUARE_LBRACE_TOKEN));
+            }
+            else if(s[cursor]==']'){
+                tokens.push_back(Token("]",SQUARE_RBRACE_TOKEN));
+            }
+            else if(s[cursor]=='('){
+                tokens.push_back(Token("(",CURVE_LBRACE_TOKEN));
+            }
+            else if(s[cursor]==')'){
+                tokens.push_back(Token(")",CURVE_RBRACE_TOKEN));
+            }
+            cursor++;
+        }
+        else if(s[cursor]==':'){
+            tokens.push_back(Token(":",COLON_TOKEN));
+            cursor++;
+        }
+
+
 
 
     }
